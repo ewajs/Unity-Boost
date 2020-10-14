d3063bee9718e3efa64ea7bb1ce32ae3c49105e4
ewajs <ezequiel.wajs@gmail.com>
2020-10-14 20:15:41 -0300

    Game finished with 4 levels

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -100,7 +100,12 @@ public class Rocket : MonoBehaviour
 
     private void LoadNextScene()
     {
-        SceneManager.LoadScene(1);
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
     }
 
     private void Thrust()
